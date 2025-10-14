e88475568e8875d5f585376047f0815c3bd522b8
Purple-Code-sh <purple.code.sh@gmail.com>
2025-10-14 16:00:45 -0600

    add cdn domain to avoid blocking in images sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "portfolio.shmin-ib.com",
+        port: "",
+        pathname: "/landings/**",
+      },
+    ],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
