0a4b5f7ab2f9caf3e6bc08d675d2567525fa5c20
Purple-Code-sh <purple.code.sh@gmail.com>
2025-11-01 16:21:00 -0600

    improve 3d model loading and rendering

--- src/app/[locale]/components/ParallaxBg.tsx
+++ src/app/[locale]/components/ParallaxBg.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from "react";
+import Head from "next/head";
 import { useTranslations } from "next-intl";
+import dynamic from "next/dynamic";
 import {
   Mouse,
   Rotate3d,
@@ -9,76 +11,92 @@ import {
   ArrowBigUp,
 } from "lucide-react";
 
-const Spline = React.lazy(() => import("@splinetool/react-spline/next"));
-
 export default function ParallaxBg() {
   const t = useTranslations("Hero.controls");
 
+  const Spline = dynamic(() => import("@splinetool/react-spline/next"), {
+    ssr: false,
+    loading: () => (
+      <div className="flex items-end lg:items-center p-8 justify-center h-full w-full text-txt-300/80">
+        {t("loading")}
+      </div>
+    ),
+  });
+
   return (
-    <section className="absolute inset-0 ">
-      <div className="relative overflow-hidden h-screen ">
-        {/* Background */}
-        <div className="bg-[url('/bg-grany-2.webp')] mix-blend-difference opacity-75 bg-cover absolute inset-0 bg-top-left bg-no-repeat -z-10" />
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="https://prod.spline.design/WlUJKj9kPo5C4Dac/scene.splinecode"
+          as="fetch"
+          crossOrigin="anonymous"
+        />
+      </Head>
 
-        <div className="w-full h-11/12 lg:w-3/4 lg:top-0 lg:right-0 absolute bottom-0">
-          <div className="relative h-full w-full bg-transparent p-8">
-            <div className="absolute bottom-4 right-4 z-30 w-auto rounded-md hidden lg:block bg-shBlack py-2 px-5 text-sm text-txt-300/60">
-              <div className="flex items-center justify-end gap-1">
-                <span>{t("hold")}</span>
-                <span className="font-bold text-txt-300/70">{t("shift")}</span>
+      <section className="absolute inset-0 ">
+        <div className="relative overflow-hidden h-screen ">
+          {/* Background */}
+          <div className="bg-[url('/bg-grany-2.webp')] mix-blend-difference opacity-75 bg-cover absolute inset-0 bg-top-left bg-no-repeat -z-10" />
 
-                <ArrowBigUp size={16} className="text-txt-200/80" />
-                <span>{t("to")}</span>
+          <div className="w-full h-11/12 lg:w-3/4 lg:top-0 lg:right-0 absolute bottom-0">
+            <div className="relative h-full w-full bg-transparent p-8">
+              <div className="absolute bottom-4 right-4 z-30 w-auto rounded-md hidden lg:block bg-shBlack py-2 px-5 text-sm text-txt-300/60">
+                <div className="flex items-center justify-end gap-1">
+                  <span>{t("hold")}</span>
+                  <span className="font-bold text-txt-300/70">
+                    {t("shift")}
+                  </span>
+
+                  <ArrowBigUp size={16} className="text-txt-200/80" />
+                  <span>{t("to")}</span>
+                </div>
+
+                <div className="my-1 border-b border-txt-200/25"></div>
+
+                <div className="flex items-center justify-end gap-2">
+                  <span>{t("panClick")}</span>
+                  <MousePointerClick size={16} className="text-txt-300/80" />
+                </div>
+
+                <div className="mt-1 flex items-center justify-end gap-2">
+                  <span>{t("zoomWheel")}</span>
+                  <Mouse size={16} className="text-txt-300/80" />
+                </div>
               </div>
 
-              <div className="my-1 border-b border-txt-200/25"></div>
+              <div className="absolute bottom-4 right-4 z-30 w-auto lg:hidden rounded-md bg-shBlack px-6 py-2 text-[9px] sm:text-xs text-txt-300/60">
+                <div className="flex items-center justify-end gap-2">
+                  <span className="font-medium text-txt-300/70">
+                    {t("orbit")}
+                  </span>
+                  <span className="text-txt-300/50">{t("twoFingers")}</span>
+                  <Rotate3d size={14} className="text-txt-300/70" />
+                </div>
 
-              <div className="flex items-center justify-end gap-2">
-                <span>{t("panClick")}</span>
-                <MousePointerClick size={16} className="text-txt-300/80" />
-              </div>
+                <div className="mt-1 flex items-center justify-end gap-2">
+                  <span className="font-medium text-txt-300/70">
+                    {t("zoom")}
+                  </span>
+                  <span className="text-txt-300/50">{t("pinching")}</span>
+                  <ZoomIn size={14} className="text-txt-300/70" />
+                </div>
 
-              <div className="mt-1 flex items-center justify-end gap-2">
-                <span>{t("zoomWheel")}</span>
-                <Mouse size={16} className="text-txt-300/80" />
-              </div>
-            </div>
-
-            <div className="absolute bottom-4 right-4 z-30 w-auto lg:hidden rounded-md bg-shBlack px-6 py-2 text-[9px] sm:text-xs text-txt-300/60">
-              <div className="flex items-center justify-end gap-2">
-                <span className="font-medium text-txt-300/70">
-                  {t("orbit")}
-                </span>
-                <span className="text-txt-300/50">{t("twoFingers")}</span>
-                <Rotate3d size={14} className="text-txt-300/70" />
-              </div>
-
-              <div className="mt-1 flex items-center justify-end gap-2">
-                <span className="font-medium text-txt-300/70">{t("zoom")}</span>
-                <span className="text-txt-300/50">{t("pinching")}</span>
-                <ZoomIn size={14} className="text-txt-300/70" />
-              </div>
-
-              <div className="mt-1 flex items-center justify-end gap-2">
-                <span className="font-medium text-txt-300/70">{t("pan")}</span>
-                <span className="text-txt-300/50">{t("threeFingers")}</span>
-                <Move size={14} className="text-txt-300/70" />
+                <div className="mt-1 flex items-center justify-end gap-2">
+                  <span className="font-medium text-txt-300/70">
+                    {t("pan")}
+                  </span>
+                  <span className="text-txt-300/50">{t("threeFingers")}</span>
+                  <Move size={14} className="text-txt-300/70" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="w-full h-11/12 lg:w-5/6 lg:top-0 lg:right-0 absolute bottom-0 z-20 brightness-110">
-          <Suspense
-            fallback={
-              <div className="flex items-end lg:items-center p-8 justify-center h-full w-full text-txt-300/80">
-                {t("loading")}
-              </div>
-            }
-          >
+          <div className="w-full h-11/12 lg:w-5/6 lg:top-0 lg:right-0 absolute bottom-0 z-20 brightness-110">
             <Spline scene="https://prod.spline.design/WlUJKj9kPo5C4Dac/scene.splinecode" />
-          </Suspense>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
